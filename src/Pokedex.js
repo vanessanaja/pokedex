ed854a02dc716d445fcad62998879e85ae569f32
vanessanaja <vanessacnaja@gmail.com>
2019-04-25 11:25:04 +0200

    add experience total

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -19,6 +19,7 @@ class Pokedex extends Component{
     return (
       <div className="Pokedex">
         <h1>Pokedex</h1>
+        <p>Total Experience {this.props.exp}</p>
         <div className="Pokedex-cards">
           {this.props.pokemon.map((p) => (
             <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
