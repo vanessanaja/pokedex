a3ef51686fde71190a831358e3714f17dc625e4f
vanessanaja <vanessacnaja@gmail.com>
2019-04-28 11:27:22 +0200

    style Pokecard background/alignment

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -9,7 +9,9 @@ class Pokecard extends Component {
     return (
       <div className= "Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src= {imgSrc} alt= {this.props.name} />
+        <div className="Pokecard-img">
+          <img src= {imgSrc} alt= {this.props.name} />
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">Exp: {this.props.exp}</div>
       </div>
